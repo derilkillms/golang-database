5897a89350a08b2e05ee62871cc4e098dd63288e
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-07 16:02:17 +0700

    database

--- database.go
+++ database.go
@@ -3,18 +3,21 @@ package golang_database
 import (
 	"database/sql"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestOpenConnection(t *testing.T) {
+func GetConnection(t *testing.T) {
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_db")
 	if err != nil {
 		panic(err)
 	}
 
-	db.SetConnMaxIdleTime(10)
-
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
 	//gunakan DB
 	defer db.Close()
 }
